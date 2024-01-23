c20421bfbb266415ae71aace446775ceb3507414
maijanp <maijapapazi@gmail.com>
2024-01-23 15:06:04 +0100

    Table base created

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,18 @@
+export const Table = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Priority</th>
+                    <th>Due Date</th>
+                    <th>Done</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                
+            </tbody>
+        </table>
+    )
+}
