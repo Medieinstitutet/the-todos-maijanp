7dd36a9105d588699fb2da3a1988b7433fbe7a3a
maijanp <maijapapazi@gmail.com>
2024-01-25 19:17:22 +0100

    order by isDone done

--- src/components/tableRow/TableRow.tsx
+++ src/components/tableRow/TableRow.tsx
@@ -1,27 +1,38 @@
-
-import { ToDo } from "../../models/ToDo"
-import styles from './TableRow.module.css'
+import { ToDo } from "../../models/ToDo";
+import styles from "./TableRow.module.css";
 interface ITableRowProps {
-    theTask: ToDo;
-    i:number;
-    onToggleStatus: (index: number) => void;
+  theTask: ToDo;
+  i: number;
+  onToggleStatus: (index: number) => void;
 }
 
-export const TableRow = ({theTask: {task, priority, deadline, isDone }, i, onToggleStatus 
+export const TableRow = ({
+  theTask: { task, priority, deadline, isDone },
+  i,
+  onToggleStatus,
 }: ITableRowProps) => {
+  const handleClick = () => {
+    onToggleStatus(i);
+  };
 
-    const handleClick = () => {
-        onToggleStatus(i)        
-    }
-
-    return (
-        <>
-        <tr className={styles.row}>
-            <td>{task}</td>
-            <td>{priority}</td>
-            <td>{deadline}</td>
-            <td> <button className={`${styles.button} ${isDone ? styles.done : styles.notDone}`}onClick={handleClick}>{isDone ? "Yep!" : "Nope!"}</button></td>
-        </tr>
-        </>
-    )
-}
+  return (
+    <>
+      <tr className={styles.row}>
+        <td>{task}</td>
+        <td>{priority}</td>
+        <td>{deadline}</td>
+        <td>
+          {" "}
+          <button
+            className={`${styles.button} ${
+              isDone ? styles.done : styles.notDone
+            }`}
+            onClick={handleClick}
+          >
+            {isDone ? "Yep!" : "Nope!"}
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
